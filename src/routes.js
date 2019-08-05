0ada66b89ecc67a61b2e46bfc1f913396b043c22
abimael andrade <abimaelc.andrade@gmail.com>
2019-08-05 09:30:12 -0300

    Install and config: eslint, prettier and editorconfig Fix: update style code

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-  return res.json({ message: "Hello Rocketseat" });
-});
+router.get('/', (req, res) => res.json({ message: 'Hello Rocketseat' }));
 
 export default router;
